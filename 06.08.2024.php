7965bc03a4188b23e91205f294911f5d91bd6f85
kamilpums <163110612+kamilpums@users.noreply.github.com>
2024-09-06 18:17:00 +0200

    06.08.24
    <!DOCTYPE html>
    <html lang="pl">
    <head>
        <meta charset="UTF-8">
        <meta name="viewport" content="width=device-width, initial-scale=1.0">
        <title>Lekcja</title>
    </head>
    <body>
        <?php
        $sigma=["Maks","Zuzia","Kuba","Noel","Aleks"];
        print_r($sigma);
        echo "<br>";
        echo $sigma[2];
        $test['imie']='Kamil';
        $test['nazwisko']='Nowak';
        $test['wiek']='18';
        print_r($test);
        echo '<br>'.$test{'imie'};
        echo "<br>";
        $dwuwymiar{0}{0}=1;
        $dwuwymiar{0}{1}=2;
        $dwuwymiar{0}{2}=3;
        $dwuwymiar{1}{0}=4;
        $dwuwymiar{1}{1}=5;
        $dwuwymiar{1}{2}=6;
        $dwuwymiar{2}{0}=7;
        $dwuwymiar{2}{1}=8;
        $dwuwymiar{2}{2}=9;
        print_r($dwuwymiar);
    
        for($i=0;$i<6;$i++){
            echo '<br>'.$sigma{$i};
        }
    
    
        ?>
    </body>
    </html>

--- 06.08.2024.php
+++ 06.08.2024.php
@@ -9,6 +9,30 @@
     <?php
     $sigma=["Maks","Zuzia","Kuba","Noel","Aleks"];
     print_r($sigma);
+    echo "<br>";
+    echo $sigma[2];
+    $test['imie']='Kamil';
+    $test['nazwisko']='Nowak';
+    $test['wiek']='18';
+    print_r($test);
+    echo '<br>'.$test{'imie'};
+    echo "<br>";
+    $dwuwymiar{0}{0}=1;
+    $dwuwymiar{0}{1}=2;
+    $dwuwymiar{0}{2}=3;
+    $dwuwymiar{1}{0}=4;
+    $dwuwymiar{1}{1}=5;
+    $dwuwymiar{1}{2}=6;
+    $dwuwymiar{2}{0}=7;
+    $dwuwymiar{2}{1}=8;
+    $dwuwymiar{2}{2}=9;
+    print_r($dwuwymiar);
+
+    for($i=0;$i<6;$i++){
+        echo '<br>'.$sigma{$i};
+    }
+
+
     ?>
 </body>
 </html>
